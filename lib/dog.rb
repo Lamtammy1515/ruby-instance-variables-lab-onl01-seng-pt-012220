59f5aca90514693e15c13deb7f7223f40ef6773f
Tammy Lam <lamtammy1515@gmail.com>
2020-02-02 00:58:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,13 @@
 class Dog 
-  def name=(this_dogs_name)
-   dog_name = this_dogs_name
+  def name=(dog_name)
+   this_dogs_name = dog_name
   end
+  
+  def name 
+    this_dogs_name
+  end 
 end 
 
 lassie = Dog.new 
-"Lassie" = this_dogs_name
+lassie.name = "Lassie"
+lassie.name
